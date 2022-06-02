1c4744e712dfbcf5f6b77c4d8b961dc5cd26957a
Prashant Deepake <deepakeprashant1@gmail.com>
2022-06-03 00:54:17 +0530

    ADDED : SIMPLE LINKED LIST PROGRAM

---
+++ src/LinkedList_Program/MyNode.java
@@ -0,0 +1,11 @@
+package LinkedList_Program;
+
+public class MyNode <K>{
+    public K key;
+    public MyNode next;
+
+    public MyNode(K key){
+        this.key=key;
+        this.next=null;
+    }
+}
