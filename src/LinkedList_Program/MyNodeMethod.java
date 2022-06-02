7aea5b380a82962d173f4809bbf15973aa35ee07
Prashant Deepake <deepakeprashant1@gmail.com>
2022-06-03 02:08:14 +0530

    ADDED : APPEND USER LINKED LIST PROGRAM

--- src/LinkedList_Program/MyNodeMethod.java
+++ src/LinkedList_Program/MyNodeMethod.java
@@ -6,34 +6,51 @@ import java.util.Scanner;
 public class MyNodeMethod {
     Scanner scan = new Scanner(System.in);
     ArrayList<MyNode> buffer = new ArrayList<>();
+
     public void addList() {
         System.out.println("Enter Node Data");
-        int firstKey= scan.nextInt();
+        int firstKey = scan.nextInt();
         MyNode<Integer> firstValue = new MyNode<>(firstKey);
         System.out.println("Enter Next Node Data");
-        int secondKey= scan.nextInt();
+        int secondKey = scan.nextInt();
         MyNode<Integer> secondValue = new MyNode<>(secondKey);
         System.out.println("Enter Node Data");
-        int thirdKey= scan.nextInt();
+        int thirdKey = scan.nextInt();
         MyNode<Integer> thirdValue = new MyNode<>(thirdKey);
 
         buffer.add(firstValue);
         buffer.add(secondValue);
         buffer.add(thirdValue);
     }
-    public void uc1Print(){
-        MyNode firstValue =buffer.get(0) ;
+
+    public void uc1Print() {
+        MyNode firstValue = buffer.get(0);
         MyNode secondValue = buffer.get(1);
         MyNode thirdValue = buffer.get(2);
         MyNode head = thirdValue;
         head.next = secondValue;
         secondValue.next = firstValue;
         firstValue.next = null;
-        while (head!=null){
-            System.out.print(head.key+" -> ");
+        while (head != null) {
+            System.out.print(head.key + " -> ");
             head = head.next;
         }
         System.out.print("null \n");
     }
 
+
+    public void appendPrint() {
+        MyNode firstValue = buffer.get(0);
+        MyNode secondValue = buffer.get(1);
+        MyNode thirdValue = buffer.get(2);
+        MyNode head = firstValue;
+        head.next = secondValue;
+        secondValue.next = thirdValue;
+        thirdValue.next = null;
+        while (head != null) {
+            System.out.print(head.key + " -> ");
+            head = head.next;
+        }
+        System.out.print("null \n");
+    }
 }
--- src/LinkedList_Program/MyNodeTest.java
+++ src/LinkedList_Program/MyNodeTest.java
@@ -3,6 +3,8 @@ package LinkedList_Program;
 import java.util.Scanner;
 
 public class MyNodeTest {
+    public static final int UC2=1;
+    public static final int UC3=2;
     public static void main(String args[]) {
         MyNode<Integer> firstValue = new MyNode<>(56);
         MyNode<Integer> secondValue = new MyNode<>(30);
@@ -20,11 +22,16 @@ public class MyNodeTest {
         MyNodeMethod myNodeObject = new MyNodeMethod();
         Scanner scan = new Scanner(System.in);
         System.out.println("1] Create New Linked List");
+        System.out.println("2] Append Linked List");
         int choice= scan.nextInt();
         switch (choice){
-            case 1:
+            case UC2:
                 myNodeObject.addList();
                 myNodeObject.uc1Print();
+                break;
+            case UC3:
+                myNodeObject.addList();
+                myNodeObject.appendPrint();
                 break;
             default:
                 System.out.println("Wrong Choice \nThank You...!");
